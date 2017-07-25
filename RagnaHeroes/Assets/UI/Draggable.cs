ed0734f8c6bb696cdec94a73a9e97921df4cf684
José Carlos S.A. Tissei <Jos Carlos S.A. Tissei>
2017-07-25 20:25:49 -0300

    drag n drop

---
+++ RagnaHeroes/Assets/UI/Draggable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public bool Dropped {get;set;}
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //TODO: store as an difference transform.position - eventData.position 
+		//to make the drag event less clunky
+
+        transform.SetParent(GameObject.Find("Canvas").GetComponent<Canvas>().transform);
+        Dropped = false;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        if(Dropped) return;
+
+        transform.SetParent(GameObject.Find("Hand").GetComponent<Image>().transform);
+    }
+}
---
+++ RagnaHeroes/Assets/UI/DropZone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler {
+    public void OnDrop(PointerEventData eventData)
+    {
+		var objectBeingDragged = eventData.pointerDrag.GetComponent<Draggable>();
+
+		if(objectBeingDragged == null) return;
+
+        objectBeingDragged.transform.SetParent(transform);
+		objectBeingDragged.Dropped = true;
+    }
+}
